2dbf069d8ce352d67a6d34cbf929a18e95506182
Dbravi <damianbravi@gmail.com>
2022-05-26 12:42:49 -0300

    Add stuff

--- js.js
+++ js.js
@@ -1,25 +1,55 @@
-const cl=console.log
+const cl = console.log
 
-let myLibrary = [];
+let myProxLectures = [];
+let myYearReads = [];
+let newBook = ""
 
-function addBookToLibrary() {
-  // do stuff here
+//Constructor
+function Book(title, author) {
+  this.title = title
+  this.author = author
 }
 
-function Book(title, author,pages,readcheck) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.marker = readcheck
-    }
-  
-  Book.prototype.info = function() {
-    return this.title
-  }
-
-  const Book1 = new Book('Lord of the rings', 'Tolkien', 623, 'read')
-  const Book2 = new Book('Aleph', 'JLB', 253, 'read')
+//Recibe libros del input
+function addBook() {
+  newBook = new Book(`${document.getElementById("lname").value}, ${document.getElementById("aname").value}`)
+  showNextBC()
+}
 
 
-  cl(Book1)
 
+//Agrega y muestra libros de "próximas lecturas"
+const showNextBC = function () {
+  myProxLectures.push(newBook)
+  const para = document.createElement("p");
+  para.innerText = newBook.title;
+  document.getElementById("proximos").append(para);
+
+  const buttonOk = document.createElement("button");
+  buttonOk.innerText = "V";
+  document.getElementById("leido").prepend(buttonOk);
+
+  const buttonDel = document.createElement("button");
+  buttonDel.innerText = "X";
+  document.getElementById("eliminar").prepend(buttonDel);
+}
+
+//Elimina libros de proximas Lecturas
+
+function removeBook() {
+  myProxLectures.pop();
+  const element = document.getElementById('proximos');
+  element.remove();
+}
+
+
+//Recibe input de proximas lecturas y pasa a Mis leidos 2022
+
+const adr = function addReadBook() {
+  myYearReads.push(this.newBook)
+  const para2 = document.createElement("h4");
+  para2.innerText = newBook.title;
+  document.getElementById("lecturas").appendChild(para2);
+}
+
+console.log(myProxLectures)
